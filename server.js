ea1b9203b7e81c38f91d56c8d522a7b4d7aadc53
Sonia Grajales <sonia.cris.grajales.ramirez@gmail.com>
2017-01-27 09:56:03 -0500

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -48,16 +49,22 @@ app.get('/todos/:id', function(req, res){
 // POST /todos
 app.post('/todos', function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');
+	db.todo.create(body).then(function(todo){
+		res.json(todo.toJSON());
+	}, function(e){
+		res.status(400).json(e);
+	});
 
-	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
 
-	body.id = todoNextId++;
-	body.description = body.description.trim();
-	todos.push(body);
+	// if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	res.json(body);
+	// body.id = todoNextId++;
+	// body.description = body.description.trim();
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -101,7 +108,8 @@ app.put('/todos/:id', function(req, res) {
 	res.json(validAttributes);
 });
 
-
-app.listen(PORT, function(){
-	console.log('Express listenint on port ' + PORT + '!');
+db.sequelize.sync().then(function(){
+	app.listen(PORT, function(){
+		console.log('Express listenint on port ' + PORT + '!');
+	});
 });
