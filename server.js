e78f28e3a9382f47160b3c54d8b9c9236b610089
Sonia Grajales <sonia.cris.grajales.ramirez@gmail.com>
2017-02-13 09:41:07 -0500

    Update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -91,28 +91,29 @@ app.delete('/todos/:id', function(req, res) {
 app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	var todoId = parseInt(req.params.id);
-	var todoItem = _.findWhere(todos, {id: todoId});
-	var validAttributes = {};
+	var attributes = {};
 
-	if(!todoItem) {
-		return res.status(404).send();
+	if(body.hasOwnProperty('completed')){
+		attributes.completed = body.completed;
 	}
 
-	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-		validAttributes.completed = body.completed;
-	} else if(body.hasOwnProperty('completed')) {
-		return res.status(404).send();
+	if(body.hasOwnProperty('description')) {
+		attributes.description = body.description
 	}
 
-	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description
-	} else if(body.hasOwnProperty('description')) {
-		return res.status(404).send();
-	}
-
-	_.extend(todoItem, validAttributes);
-
-	res.json(validAttributes);
+	db.todo.findById(todoId).then(function(todo){
+		if(todo){
+			todo.update(attributes).then(function(todo){
+				res.json(todo.toJSON());
+			}, function(e){
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function(){
+		res.status(500).send();
+	})
 });
 
 db.sequelize.sync().then(function(){
