3b6b6ba99685e57eade733c99d03101635bb2f02
Sonia Grajales <sonia.cris.grajales.ramirez@gmail.com>
2017-05-08 19:19:34 -0500

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -123,6 +124,17 @@ app.post('/users', function(req, res){
 		res.json(user.toPublicJSON());
 	}, function(e){
 		res.status(400).json(e);
+	});
+});
+
+// POST /users/login
+app.post('/users/login', function(req, res){
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function(user){
+		res.json(user.toPublicJSON());
+	}, function(e){
+		res.status(401).json(e);
 	});
 });
 
